dacbdb7591761a95fa8926d11fbc661350344b36
yangxin <yangxin@pingcap.com>
2022-11-15 11:23:40 +0800

    polish cluster command

--- internal/cli/cluster/list.go
+++ internal/cli/cluster/list.go
@@ -1,18 +1,35 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cluster
 
 import (
-	"encoding/json"
 	"fmt"
+	"math"
 
+	"tidbcloud-cli/internal/flag"
 	"tidbcloud-cli/internal/openapi"
+	"tidbcloud-cli/internal/output"
 	"tidbcloud-cli/internal/util"
 
 	clusterApi "github.com/c4pt0r/go-tidbcloud-sdk-v1/client/cluster"
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/spf13/cobra"
 )
 
 func ListCmd() *cobra.Command {
-	var deleteCmd = &cobra.Command{
+	var listCmd = &cobra.Command{
 		Use:     "list <projectID>",
 		Short:   "List all clusters in a project.",
 		Args:    util.RequiredArgs("projectID"),
@@ -23,21 +40,66 @@ func ListCmd() *cobra.Command {
 			pID := args[0]
 
 			params := clusterApi.NewListClustersOfProjectParams().WithProjectID(pID)
+			var total int64 = math.MaxInt64
+			var page int64 = 1
+			var pageSize int64 = defaultPageSize
+			var items []*clusterApi.ListClustersOfProjectOKBodyItemsItems0
+			for (page-1)*pageSize < total {
+				clusters, err := apiClient.Cluster.ListClustersOfProject(params.WithPage(&page).WithPageSize(&pageSize))
+				if err != nil {
+					return err
+				}
 
-			cluster, err := apiClient.Cluster.ListClustersOfProject(params)
+				total = *clusters.Payload.Total
+				page += 1
+				items = append(items, clusters.Payload.Items...)
+			}
+
+			format, err := cmd.Flags().GetString(flag.Output)
 			if err != nil {
 				return err
 			}
 
-			v, err := json.MarshalIndent(cluster, "", "  ")
-			if err != nil {
-				return err
+			if format == output.JsonFormat {
+				err := output.PrintJson(items)
+				if err != nil {
+					return err
+				}
+			} else if format == output.HumanFormat {
+				columns := []table.Column{
+					{Title: "ID", Width: 20},
+					{Title: "Name", Width: 20},
+					{Title: "Status", Width: 10},
+					{Title: "Version", Width: 10},
+					{Title: "Region", Width: 10},
+					{Title: "Type", Width: 10},
+				}
+
+				var rows []table.Row
+				for _, item := range items {
+					rows = append(rows, table.Row{
+						*(item.ID),
+						item.Name,
+						item.Status.ClusterStatus,
+						item.Status.TidbVersion,
+						item.Region,
+						item.ClusterType,
+					})
+				}
+
+				err := output.PrintHumanTable(columns, rows)
+				if err != nil {
+					return err
+				}
+				return nil
+			} else {
+				return fmt.Errorf("unsupported output format: %s", format)
 			}
 
-			fmt.Println(string(v))
 			return nil
 		},
 	}
 
-	return deleteCmd
+	listCmd.Flags().StringP(flag.Output, flag.OutputShort, output.HumanFormat, "Output format. One of: human, json, default: human")
+	return listCmd
 }
---
+++ internal/output/output.go
@@ -0,0 +1,67 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package output
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"tidbcloud-cli/internal/ui"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	JsonFormat  string = "json"
+	HumanFormat string = "human"
+)
+
+func PrintJson(items interface{}) error {
+	v, err := json.MarshalIndent(items, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(v))
+	return nil
+}
+
+func PrintHumanTable(columns []table.Column, rows []table.Row) error {
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+
+	m := ui.InitialTableModel(t)
+	if err := tea.NewProgram(m).Start(); err != nil {
+		return err
+	}
+
+	return nil
+}
