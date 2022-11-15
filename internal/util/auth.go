cdde1e6e714c1244a37c5606182c2da5d6db90ad
yangxin <yangxin@pingcap.com>
2022-11-15 16:20:49 +0800

    rename files

--- internal/util/auth.go
+++ internal/util/auth.go
@@ -23,7 +23,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-func CheckAuth(cmd *cobra.Command, args []string) error {
+func CheckAuth(*cobra.Command, []string) error {
 	curP := viper.Get(prop.CurProfile)
 	if curP == nil {
 		return fmt.Errorf("no active profile for auth, please use `config init` to create one")
