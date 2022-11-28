644b4943f864cb273eacae1a5a51f181895a6e44
Leon Yang <yangxin@pingcap.com>
2022-11-28 16:04:38 +0800

    disable table row focus, select (#17)

--- internal/ui/table_model.go
+++ internal/ui/table_model.go
@@ -37,26 +37,7 @@ func InitialTableModel(t table.Model) TableModel {
 func (m TableModel) Init() tea.Cmd { return nil }
 
 func (m TableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "esc":
-			if m.table.Focused() {
-				m.table.Blur()
-			} else {
-				m.table.Focus()
-			}
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			return m, tea.Batch(
-				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
-			)
-		}
-	}
-	m.table, cmd = m.table.Update(msg)
-	return m, cmd
+	return m, tea.Quit
 }
 
 func (m TableModel) View() string {
