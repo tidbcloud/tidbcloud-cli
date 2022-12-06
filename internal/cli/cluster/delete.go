16873806cbc78358a8819520f4ae5bcc6cb3c3d2
Leon Yang <yangxin@pingcap.com>
2022-12-06 12:18:59 +0800

    fix typo in --force hint (#46)

--- internal/cli/cluster/delete.go
+++ internal/cli/cluster/delete.go
@@ -173,7 +173,7 @@ func DeleteCmd(h *internal.Helper) *cobra.Command {
 		},
 	}
 
-	deleteCmd.Flags().BoolVar(&force, flag.Force, false, "Delete a database without confirmation")
+	deleteCmd.Flags().BoolVar(&force, flag.Force, false, "Delete a cluster without confirmation")
 	deleteCmd.Flags().StringP(flag.ProjectID, flag.ProjectIDShort, "", "The project ID of the cluster to be deleted.")
 	deleteCmd.Flags().StringP(flag.ClusterID, flag.ClusterIDShort, "", "The ID of the cluster to be deleted.")
 	return deleteCmd
