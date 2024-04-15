6aa7214dd11ed8014a92832a16917f40d21d15e0
shiyuhang <1136742008@qq.com>
2024-04-15 10:51:11 +0800

    typo

--- gen_doc.go
+++ gen_doc.go
@@ -25,8 +25,8 @@ import (
 
 func main() {
 	h := &internal.Helper{}
-	kubectl := cli.RootCmd(h)
-	err := doc.GenMarkdownTree(kubectl, "./docs/generate_doc")
+	ticloud := cli.RootCmd(h)
+	err := doc.GenMarkdownTree(ticloud, "./docs/generate_doc")
 	if err != nil {
 		log.Fatal(err)
 	}
